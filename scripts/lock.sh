be9e5bc9247725f260c8077f5085af2eaaf6b3ca
MWidzi <mlydzy123@gmail.com>
2025-11-02 15:24:18 +0100

    added scratchpads, changed cursor, updated rules and wlogout animations a bit

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 0.1
+
 hyprshot -s -zm output -m "DP-3" -o ~/Dotfiles/images -f "hyprlock_wallpaper2.png" &
 hyprshot -s -zm output -m "DP-1" -o ~/Dotfiles/images -f "hyprlock_wallpaper.png" &
 hyprshot -s -zm output -m "HDMI-A-1" -o ~/Dotfiles/images -f "hyprlock_wallpaper3.png" &
@@ -7,6 +9,6 @@ powerprofilesctl set 'power-saver' &
 
 wait
 
-sleep 1.1
+sleep 1.4
 hyprlock
 powerprofilesctl set 'balanced'
