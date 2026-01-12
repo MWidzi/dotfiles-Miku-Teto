c0da0154b045fedb680c7ad0d4408dbff9029755
Maciej Widzinski <mlydzy123@gmail.com>
2026-01-12 08:33:08 +0100

    improved split organizer

--- nvim/init.lua
+++ nvim/init.lua
@@ -152,6 +152,7 @@ end
 function AutoLayout()
     local api = require 'nvim-tree.api'
     local normal_buffers = {}
+    local terminal_buffers = {}
 
     if api.tree.is_visible() then
         local windows = vim.api.nvim_list_wins()
@@ -161,8 +162,12 @@ function AutoLayout()
             local buf_name = vim.api.nvim_buf_get_name(buf)
             local buf_type = vim.api.nvim_buf_get_option(buf, 'buftype')
 
-            if buf_name ~= '' and (buf_type == 'nofile' or buf_type == 'nowrite') then
-                vim.api.nvim_set_current_win(win)
+            vim.api.nvim_set_current_win(win)
+
+            if buf_type == 'terminal' then
+                table.insert(terminal_buffers, win)
+            elseif buf_name ~= '' and (buf_type == 'nofile' or buf_type == 'nowrite') then
+                vim.print(buf_name)
                 vim.cmd 'wincmd K'
                 if string.find(buf_name, 'diffpanel') then
                     vim.cmd 'resize 10'
@@ -173,7 +178,6 @@ function AutoLayout()
                     vim.cmd 'resize 20'
                 end
             elseif buf_name ~= '' and (buf_type == '' or buf_type == 'normal') then
-                vim.api.nvim_set_current_win(win)
                 vim.cmd 'wincmd L'
                 normal_buffers[#normal_buffers + 1] = win
             end
@@ -184,6 +188,27 @@ function AutoLayout()
         if #normal_buffers > 1 then
             vim.api.nvim_set_current_win(normal_buffers[#normal_buffers])
             vim.cmd 'vertical resize 50'
+        end
+
+        if #terminal_buffers > 0 then
+            vim.api.nvim_set_current_win(terminal_buffers[1])
+            vim.cmd 'wincmd L'
+
+            if #terminal_buffers > 1 then
+                for i = 2, #terminal_buffers do
+                    local term_buf_to_move = vim.api.nvim_win_get_buf(terminal_buffers[i])
+                    vim.api.nvim_set_current_win(terminal_buffers[1])
+                    vim.cmd 'split'
+                    vim.api.nvim_win_set_buf(0, term_buf_to_move)
+                end
+
+                for i = 2, #terminal_buffers do
+                    vim.api.nvim_win_close(terminal_buffers[i], false)
+                end
+            end
+
+            vim.cmd 'wincmd 10l'
+            vim.cmd 'vertical resize 35'
         end
     end
 end
